65a5f90c2de571bd36555e60c371c0bb5342d4a3
bikash007-gb <bikashmali12@gmail.com>
2021-05-11 15:16:40 +0530

    api views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,12 @@
 #from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from profiles_api import serializers
 # Create your views here.
 class HelloView(APIView):
     """TEst Api view"""
+    serializer_class = serializers.HelloSerializer
     def get(self,request,format=None):
         """Return a list of APIView feature"""
         an_apiview =[
@@ -15,3 +17,23 @@ class HelloView(APIView):
         ]
 
         return Response({'message':'hello','an_apiview':an_apiview})
+    
+    def post(self,request):
+        """create Hello request with our name"""
+        serializer = self.serializer_class(data=request.data)
+        if(serializer.is_valid()):
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({"message":message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)    
+            
+    def put(self,request,pk=None):
+        """handle put request"""
+        return Response({'method':'PUT'})
+    def patch(self,request,pk=None):
+        """handle partial update"""
+        return Response({'method':'PATCH'})
+    def delete(self,request,pk=None):
+        """handle delete"""
+        return Response({'method':'delete'})
